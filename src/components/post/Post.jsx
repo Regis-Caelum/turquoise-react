45f2484ab93405af959412a703aa0a2adddf662c
Regis-Caelum <khanmf@rknec.edu>
2022-05-24 03:06:14 +0530

    Fixed posts component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,18 +7,11 @@ export default function Post({ post }) {
     <div className="post">
       {<img className="postImg" src={PF} alt="" />}
       <div className="postInfo">
-        {/*<div className="postCats">*/}
-        {/*  {post.categories.map((c) => (*/}
-        {/*    <span className="postCat">{c.name}</span>*/}
-        {/*  ))}*/}
-        {/*</div>*/}
         <Link to={`/post/${post.id}`} className="link">
           <span className="postTitle">{post.title}</span>
         </Link>
         <hr />
-        {/*<span className="postDate">*/}
-        {/*  {new Date(post.createdAt).toDateString()}*/}
-        {/*</span>*/}
+
       </div>
       <p className="postDesc">{post.content}</p>
     </div>
